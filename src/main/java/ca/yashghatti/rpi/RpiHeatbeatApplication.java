3b1ccd8f4e3f979c45a070371d9440af5e2f7f45
Yash <yashghatti@gmail.com>
2025-03-26 00:26:45 -0600

    initial commit

---
+++ src/main/java/ca/yashghatti/rpi/RpiHeatbeatApplication.java
@@ -0,0 +1,13 @@
+package ca.yashghatti.rpi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpiHeatbeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpiHeatbeatApplication.class, args);
+	}
+
+}
